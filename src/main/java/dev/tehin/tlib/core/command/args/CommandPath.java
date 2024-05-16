ba88fa3aa27455176e697a91e113cefc5d19f9e8
Cohinz <62246450+Tehin@users.noreply.github.com>
2024-05-16 17:08:31 -0500

    fix: bigger jar than expected

--- src/main/java/dev/tehin/tlib/core/command/args/CommandPath.java
+++ src/main/java/dev/tehin/tlib/core/command/args/CommandPath.java
@@ -1,37 +1,38 @@
 package dev.tehin.tlib.core.command.args;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Arrays;
 import java.util.Locale;
 
+@Getter
 public class CommandPath {
 
+    @Getter(AccessLevel.NONE)
     private final String[] args;
 
-    @Getter
     private final String asString;
 
-    @Getter
-    private final String[] subCommands;
+    private String[] subCommands = null;
+
+    private @Setter boolean subCommand = false;
 
     public CommandPath(String path) {
         this.args = path.split("\\.");
 
+        this.setSubCommand(args.length > 0);
+
         this.asString = path.toLowerCase(Locale.ROOT);
 
         if (isSubCommand()) subCommands = Arrays.copyOfRange(args, 1, args.length);
-        else subCommands = null;
     }
 
     public static CommandPath parse(String[] args) {
         String join = String.join(".", args);
 
         return new CommandPath(join);
-    }
-
-    public boolean isSubCommand() {
-        return args.length > 1;
     }
 
     public String getParentCommand() {
