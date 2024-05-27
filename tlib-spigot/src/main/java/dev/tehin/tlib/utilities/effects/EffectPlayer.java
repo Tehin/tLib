c7136e6c7ff46c40cf3a973b2cb00fadf118484e
Cohinz <62246450+Tehin@users.noreply.github.com>
2024-05-27 16:50:41 -0500

    Update EffectPlayer.java

--- tlib-spigot/src/main/java/dev/tehin/tlib/utilities/effects/EffectPlayer.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/utilities/effects/EffectPlayer.java
@@ -17,7 +17,7 @@ public class EffectPlayer {
     private final EffectBuilder builder;
 
     public void normal(Location location) {
-        final double viewing = builder.viewingRadius();
+        final double viewing = builder.getViewingRadius();
 
         Collection<Entity> entities = location.getWorld().getNearbyEntities(location, viewing, viewing, viewing);
 
@@ -26,23 +26,23 @@ public class EffectPlayer {
                 .map(entity -> (Player) entity)
                 .collect(Collectors.toList());
 
-        RGB rgb = builder.rgb();
+        RGB rgb = builder.getRgb();
 
         for (Player player : players) {
             player.spigot().playEffect(location,
-                    builder.effect(),
+                    builder.getEffect(),
                     0,
-                    builder.data(),
+                    builder.getData(),
                     rgb.getR() / 255, rgb.getG() / 255, rgb.getB() / 255,
-                    builder.speed(),
-                    builder.particlesPerPacket(),
-                    builder.viewingRadius()
+                    builder.getSpeed(),
+                    builder.getParticlesPerPacket(),
+                    builder.getViewingRadius()
             );
         }
     }
 
     public void circle(Location location, double radius, double angle) {
-        for (double currentAngle = 0; currentAngle < 360; currentAngle += 360.0 / builder.particlesPerEffect()) {
+        for (double currentAngle = 0; currentAngle < 360; currentAngle += 360.0 / builder.getParticlesPerEffect()) {
             double asOffset = currentAngle + angle;
             double x = location.getX() + radius * Math.cos(Math.toRadians(asOffset));
             double z = location.getZ() + radius * Math.sin(Math.toRadians(asOffset));
