522ca7aa285e1a0231ae3caeba67b5756a44850e
Cohinz <62246450+Tehin@users.noreply.github.com>
2024-05-28 20:19:02 -0500

    fix[spigot]: default message to no permission cmd

--- tlib-spigot/src/main/java/dev/tehin/tlib/api/command/annotation/CommandCompletion.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/api/command/annotation/CommandCompletion.java
@@ -2,6 +2,10 @@ package dev.tehin.tlib.api.command.annotation;
 
 import org.bukkit.command.TabCompleter;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CommandCompletion {
 
     Class<? extends TabCompleter> value();
--- tlib-spigot/src/main/java/dev/tehin/tlib/api/command/annotation/CommandMessaging.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/api/command/annotation/CommandMessaging.java
@@ -6,5 +6,5 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CommandMessaging {
     String usage();
-    String noPermission();
+    String noPermission() default "&cYou don't have enough permission to do this";
 }
