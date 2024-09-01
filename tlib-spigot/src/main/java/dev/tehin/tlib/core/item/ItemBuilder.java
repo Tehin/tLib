77d73a8121441fe8e1a5b7dfc9b9ef378066b86c
Tehin <62246450+Tehin@users.noreply.github.com>
2024-09-01 08:55:19 -0500

    add enchant with book support to item builder

--- tlib-spigot/src/main/java/dev/tehin/tlib/core/item/ItemBuilder.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/core/item/ItemBuilder.java
@@ -3,18 +3,22 @@ package dev.tehin.tlib.core.item;
 import dev.tehin.tlib.api.menu.action.MenuAction;
 import dev.tehin.tlib.utilities.MessageUtil;
 import dev.tehin.tlib.utilities.item.ItemUtil;
-import lombok.Getter;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -30,19 +34,36 @@ public class ItemBuilder {
     private boolean glow = false;
     private MenuAction action = null;
 
+    @Setter(AccessLevel.NONE)
+    private Map<Enchantment, Integer> enchants = null;
+
     public ItemStack build() {
-        ItemStack base = new ItemStack(material, amount, (short) data);
-        setMeta(base);
+        ItemStack base = new ItemStack(material, amount);
+        setProperties(base);
 
         return base;
     }
 
     public void apply(ItemStack found) {
-        setMeta(found);
+        setProperties(found);
     }
 
-    private void setMeta(ItemStack item) {
+    private void setProperties(ItemStack item) {
+        item.setDurability((short) data);
+
         ItemMeta meta = item.getItemMeta();
+
+        if (enchants != null) {
+            // If we are adding enchants to an enchanted book, we need to use the EnchantmentStorageMeta
+            if (meta instanceof EnchantmentStorageMeta enchantMeta) {
+                for (Map.Entry<Enchantment, Integer> enchantment : enchants.entrySet()) {
+                    enchantMeta.addStoredEnchant(enchantment.getKey(), enchantment.getValue(), true);
+                }
+            } else {
+                // If not, add the enchants to the item directly
+                enchants.forEach(item::addUnsafeEnchantment);
+            }
+        }
 
         if (name != null) {
             meta.setDisplayName(MessageUtil.color(name));
@@ -60,6 +81,12 @@ public class ItemBuilder {
         item.setItemMeta(meta);
 
         if (glow) ItemUtil.addGlow(item);
+    }
+
+    public void addEnchant(Enchantment enchantment, int level) {
+        if (enchants == null) enchants = new HashMap<>();
+
+        enchants.put(enchantment, level);
     }
 
     public Material getMaterial() {
