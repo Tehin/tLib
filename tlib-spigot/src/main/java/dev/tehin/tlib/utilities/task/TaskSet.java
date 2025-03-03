219f5006c7e07583ace4cc0cfbfb064c768588c6
Tehin <62246450+Tehin@users.noreply.github.com>
2025-03-03 08:57:34 -0500

    rework task set with bukkit scheduler

--- tlib-spigot/src/main/java/dev/tehin/tlib/utilities/task/TaskSet.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/utilities/task/TaskSet.java
@@ -1,8 +1,10 @@
 package dev.tehin.tlib.utilities.task;
 
+import dev.tehin.tlib.api.tLib;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.*;
 import java.util.function.Supplier;
@@ -10,16 +12,33 @@ import java.util.function.Supplier;
 public class TaskSet {
 
     // Sets that use queues for overlap prevention
-    @Getter
-    private static final Map<UUID, Integer> queues = new HashMap<>();
+    private static final Map<UUID, List<TaskSet>> QUEUES = new HashMap<>();
 
-    private final List<TaskEntry> tasks = new LinkedList<>();
+    private final Map<Integer, TaskEntry> tasks = new HashMap<>();
+
+    private BukkitTask task = null;
+    private int duration = 0;
 
     private Supplier<Boolean> cancelIf;
     private Runnable onCancel;
 
+    public static void stop(Player player) {
+        List<TaskSet> set = QUEUES.remove(player.getUniqueId());
+        if (set == null) return;
+
+        set.forEach(TaskSet::cancel);
+    }
+
+    private void cancel() {
+        if (task != null) {
+            task.cancel();
+            task = null;
+        }
+    }
+
     public TaskSet at(int time, Runnable task) {
-        tasks.add(new TaskEntry(time, task));
+        tasks.put(time, new TaskEntry(time, task));
+        duration += time;
         return this;
     }
 
@@ -34,80 +53,65 @@ public class TaskSet {
     }
 
     public int getTotalDuration() {
-        return tasks.stream().mapToInt(TaskEntry::delay).sum();
+        return duration;
     }
 
     // TODO: Tomar en cuenta si las tareas se dan en diferentes timings (reducir la duration conforme pasan los ticks)
     public void queueRun(Player player, int endOffset) {
-        int totalDuration = getTotalDuration() + endOffset + 1;
+        Runnable task = this::runSync;
 
-        Runnable task = () -> {
-            runSync();
+        int playerDelay = 0;
 
-            // Clear this set from the queue after it's finished
-            TaskUtil.runSyncLater(() -> {
-                queues.computeIfPresent(player.getUniqueId(), (k, v) -> {
-                    int result = v - totalDuration;
-                    if (result <= 0) return null;
+        if (QUEUES.containsKey(player.getUniqueId())) {
+            playerDelay = QUEUES.get(player.getUniqueId()).stream().mapToInt(TaskSet::getTotalDuration).sum();
+        }
 
-                    return result;
-                });
-            }, totalDuration);
-        };
-
-        TaskUtil.runSyncLater(task, queues.getOrDefault(player.getUniqueId(), 0));
-
-        queues.compute(player.getUniqueId(), (k, v) -> {
-            if (v == null) v = 0;
-
-            return v + totalDuration;
-        });
+        // TODO: Implement the end offset
+        TaskUtil.runSyncLater(task, playerDelay);
     }
 
     public void runSync() {
-        for (TaskEntry entry : tasks) {
-            schedule(entry, false);
-        }
+        this.task = createTask().runTaskTimer(tLib.get().getOwner(), 0, 1);
     }
 
     public void runAsync() {
-        for (TaskEntry task : tasks) {
-            schedule(task, true);
-        }
+        this.task = createTask().runTaskTimerAsynchronously(tLib.get().getOwner(), 0, 1);
     }
 
-    private Runnable getRunnable(Runnable entry) {
+    private BukkitRunnable createTask() {
+        return new BukkitRunnable() {
+
+            private int currentTick = 0;
+
+            @Override
+            public void run() {
+                if (currentTick > duration) {
+                    this.cancel();
+                    return;
+                }
+
+                TaskEntry entry = tasks.get(currentTick++);
+                if (entry == null) return;
+
+                injectCancelOnRunnable(entry.task()).run();
+            }
+        };
+    }
+
+    private Runnable injectCancelOnRunnable(Runnable entry) {
         return () -> {
             if (cancelIf != null && cancelIf.get()) {
                 if (onCancel != null) {
                     onCancel.run();
                     onCancel = null;
+
+                    this.task.cancel();
                 }
                 return;
             }
 
             entry.run();
         };
-    }
-
-    private void schedule(TaskEntry entry, boolean async) {
-        Runnable task = getRunnable(entry.task());
-
-        if (entry.delay() == 0) {
-            if (async) {
-                TaskUtil.runAsync(task);
-            } else {
-                TaskUtil.runSync(task);
-            }
-
-            return;
-        }
-
-        if (async) {
-            TaskUtil.runAsyncLater(getRunnable(entry.task()), entry.delay());
-        } else {
-            TaskUtil.runSyncLater(getRunnable(entry.task()), entry.delay());
-        }
     }
 
 }
