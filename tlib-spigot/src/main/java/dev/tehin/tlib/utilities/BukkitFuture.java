857f1c5b9bffa402fea28aaf52a5f086c41b3bb6
Tehin <62246450+Tehin@users.noreply.github.com>
2025-03-13 20:05:20 -0500

    fix no execution on supplyAsync in BukkitFuture

--- tlib-spigot/src/main/java/dev/tehin/tlib/utilities/BukkitFuture.java
+++ tlib-spigot/src/main/java/dev/tehin/tlib/utilities/BukkitFuture.java
@@ -1,7 +1,7 @@
 package dev.tehin.tlib.utilities;
 
 import dev.tehin.tlib.utilities.task.TaskUtil;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -10,19 +10,39 @@ import java.util.function.Supplier;
 @RequiredArgsConstructor
 public class BukkitFuture<T> {
 
+    // A result was given but the completion callback has not been assigned
+    @AllArgsConstructor
+    @Getter
+    private static class PendingResult<T> {
+        T result;
+        Throwable throwable;
+    }
+
     private BiConsumer<T, Throwable> consumer;
+    private PendingResult<T> pending;
+
+    // Whether the task is expected to run async
+    private boolean async;
 
     public void complete(T object) {
         try {
-            if (consumer != null) consumer.accept(object, null);
+            if (consumer != null) {
+                consumer.accept(object, null);
+            } else {
+                this.pending = new PendingResult<>(object, null);
+            }
         } catch (Exception e) {
-            e.printStackTrace();
-            consumer.accept(null, e);
+            if (consumer != null) {
+                consumer.accept(null, e);
+            } else {
+                this.pending = new PendingResult<>(null, e);
+            }
         }
     }
 
     public static <T> BukkitFuture<T> supplyAsync(Supplier<T> task) {
         BukkitFuture<T> future = new BukkitFuture<>();
+        future.async = true;
 
         Runnable result = () -> future.complete(task.get());
         TaskUtil.runAsync(result);
@@ -32,6 +52,7 @@ public class BukkitFuture<T> {
 
     public static <T> BukkitFuture<T> supplySync(Supplier<T> task) {
         BukkitFuture<T> future = new BukkitFuture<>();
+        future.async = false;
 
         Runnable result = () -> future.complete(task.get());
         TaskUtil.runSync(result);
@@ -41,6 +62,18 @@ public class BukkitFuture<T> {
 
     public BukkitFuture<T> whenComplete(BiConsumer<T, Throwable> consumer) {
         this.consumer = consumer;
+
+        // A pending result is waiting, execute it now
+        if (pending != null) {
+            Runnable task = () -> consumer.accept(pending.getResult(), pending.getThrowable());
+
+            if (async) {
+                TaskUtil.runAsync(task);
+            } else {
+                TaskUtil.runSync(task);
+            }
+        }
+
         return this;
     }
 
